7c585eff507dac9ac45dd151845f8e8a27fc738e
GodsfavourNnaji <godsfavournnaji@gmail.cmm>
2022-07-28 15:54:28 +0100

    Notes Model Created

--- animalib/account/forms.py
+++ animalib/account/forms.py
@@ -1,6 +1,8 @@
+from dataclasses import fields
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from .models import Notes
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -11,3 +13,9 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ("username","email")
+
+
+class NotesForm(forms.ModelForm):
+    class Meta:
+        model = Notes
+        fields = ('title', 'body')
--- animalib/account/models.py
+++ animalib/account/models.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -8,3 +9,24 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Notes(models.Model):
+    anim_name = models.ForeignKey('Anim_name',on_delete=models.CASCADE,)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True,editable=False)
+    modifed = models.DateTimeField(auto_now=True) 
+
+    class Meta:
+        ordering = ['created',]
+
+    def __str__(self):
+        return 'My Note on {}'.format(self.anim_name)
+
+
+
+# GODSFAVOUR COMMENTS
+# there is still a need to create & edit the animation template 
+# to display the notes and a form to add a new note to it
+
+# The model returns an error as 'anim_name' model has't been created yet
+# and is linked to the model as a FK
